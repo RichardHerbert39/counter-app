b60ec6bcba225b60a8b2417bfb0a0ca19c7112ea
RichardHerbert39 <richyherbert@hotmail.com>
2019-08-20 16:42:57 +0100

    Created counter app from tutorial

---
+++ src/components/navbar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const NavBar = ({ totalCounters }) => {
+	return (
+		<nav className="navbar navbar-light bg-light">
+			<span className="navbar-brand" href="#">
+				Navbar{" "}
+				<span className="badge badge-pill badge-secondary">
+					{totalCounters}
+				</span>
+			</span>
+		</nav>
+	);
+};
+
+export default NavBar;
